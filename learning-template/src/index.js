b62c3de386ab3d14dc64e63ce858b280aed21da4
xbmchina <1131663064@163.com>
2019-02-23 19:31:20 +0800

    template

---
+++ learning-template/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal Rfsdfsdeact Webpack 23232323Babel Setup00086787';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
